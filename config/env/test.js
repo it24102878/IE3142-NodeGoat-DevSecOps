5cf720e4c48d431bd486e0981ff27162100f0afb
IT24102878 <IT24102878@my.sliit.lk>
2026-09-24 16:55:48 +0530

    security: externalize runtime secrets and configure Gitleaks gate

--- config/env/test.js
+++ config/env/test.js
@@ -1,8 +1,6 @@
 module.exports = {
-   // If you want to debug regression tests, you will need the following.
-   zapHostName: "192.168.56.20",
-   zapPort: "8080",
-   // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
-   zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
-   zapApiFeedbackSpeed: 5000 // Milliseconds.
+   zapHostName: process.env.ZAP_HOST || "192.168.56.20",
+   zapPort: process.env.ZAP_PORT || "8080",
+   zapApiKey: process.env.ZAP_API_KEY || "",
+   zapApiFeedbackSpeed: 5000
 };
